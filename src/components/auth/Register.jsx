3fc4bb6c20295a313ec401acf58f668ed988215b
Imanul Aufa <imanulaufa@gmail.com>
2025-05-15 11:33:59 +0700

    fix(register) : fix confirm password hide and button hover

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router";
-import registerBG from "/registerBG.svg"
+import { Link } from "react-router";
+import registerBG from "/registerBG.svg";
 import logo from "/Logo.svg";
-import profileIcon from "/icons/Profile.svg"
+import profileIcon from "/icons/Profile.svg";
 import mail from "/icons/mail.svg";
 import password from "/icons/Password.svg";
 import eyeslash from "/icons/EyeSlash.svg";
@@ -12,16 +12,17 @@ import googleIcon from "/icons/google_icon.svg";
 
 function Register() {
   const [showPassword, setShowPassword] = useState(false);
+  const [showCheckPassword, setCheckShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const navigate = useNavigate()
+  const toggleCheckPasswordVisibility = () => {
+    setCheckShowPassword(!showCheckPassword);
+  };
 
   return (
-
-    
     <>
       <main className="flex gap-5 max-lg:bg-[url(/registerBG.svg)] bg-no-repeat bg-cover lg:flex">
         <img src={registerBG} className="max-lg:hidden" />
@@ -36,7 +37,11 @@ function Register() {
                 Full Name
               </label>
               <div className="flex gap-3.5 p-3.5 mt-3.5 border rounded-md bg-[#FCFDFE]">
-                <img src={profileIcon} alt="full name" className="cursor-pointer" />
+                <img
+                  src={profileIcon}
+                  alt="full name"
+                  className="cursor-pointer"
+                />
                 <input
                   type="text"
                   name="fullname"
@@ -76,8 +81,12 @@ function Register() {
                   placeholder="Enter Your Password"
                   className="outline-none w-full h-full"
                 />
-                <img src={showPassword ? eyeslash : eye} alt={showPassword ? "show password" : "hide password"} 
-                onClick={togglePasswordVisibility} className="cursor-pointer"/>
+                <img
+                  src={showPassword ? eyeslash : eye}
+                  alt={showPassword ? "show password" : "hide password"}
+                  onClick={togglePasswordVisibility}
+                  className="cursor-pointer"
+                />
               </div>
             </div>
             {/* CONFIRM PASSWORD*/}
@@ -88,23 +97,30 @@ function Register() {
               <div className="flex gap-3.5 p-3.5 mt-3.5 border rounded-md bg-[#FCFDFE]">
                 <img src={password} alt="confirm password" />
                 <input
-                  type={showPassword ? "text" : "password"}
+                  type={showCheckPassword ? "text" : "password"}
                   name="confirmPassword"
                   id="confirmPassword"
                   placeholder="Enter Your Password Again"
                   className="outline-none w-full h-full"
                 />
-                <img src={showPassword ? eyeslash : eye} alt={showPassword ? "show password" : "hide password"} 
-                onClick={togglePasswordVisibility} className="cursor-pointer"/>
+                <img
+                  src={showCheckPassword ? eyeslash : eye}
+                  alt={showCheckPassword ? "show password" : "hide password"}
+                  onClick={toggleCheckPasswordVisibility}
+                  className="cursor-pointer"
+                />
               </div>
             </div>
-            
-            <button className="w-full py-2.5 rounded-md bg-(--secondary-color) hover:scale-[1.05] cursor-pointer">
+
+            <button className="w-full py-2.5 rounded-md bg-(--secondary-color) hover:scale-[0.9] hover:text-white duration-500 [transition-timing-function:cubic-bezier(0.34,1.56,0.64,1)] cursor-pointer">
               Register
             </button>
             <p className="text-center">
               Have An Account?{" "}
-              <Link to="/auth" className="text-(--secondary-color) cursor-pointer">
+              <Link
+                to="/auth"
+                className="text-(--secondary-color) cursor-pointer hover:font-semibold"
+              >
                 Login
               </Link>
             </p>
@@ -114,13 +130,29 @@ function Register() {
               <div className="border-b-1 border-black/50 translate-y-2 h-1 w-2/5" />
             </div>
             <div className="flex justify-evenly">
-              <div className="bg-white/80 p-5 flex gap-2.5 justify-center rounded-[15px] shadow-[0_2px_10px_rgba(0,0,0,0.4)] hover:scale-[1.05] cursor-pointer lg:w-2/5 lg:order-2 lg:p-3">
-                <img src={googleIcon} alt="google" />
-                <p className="max-lg:hidden">Facebook</p>
+              <div className="relative bg-white/80 p-5 flex gap-2.5 justify-center rounded-[15px] shadow-[0_2px_10px_rgba(0,0,0,0.4)] overflow-hidden group hover:scale-[0.9] duration-500 [transition-timing-function:cubic-bezier(0.34,1.56,0.64,1)] cursor-pointer lg:w-2/5 lg:order-2 lg:p-3">
+                <img
+                  src={googleIcon}
+                  alt="google"
+                  className="transition-all duration-500"
+                />
+                <p
+                  className="max-lg:hidden"
+                >
+                  Google
+                </p>
               </div>
-              <div className="bg-white/80 p-5 flex gap-2.5 justify-center rounded-[15px] shadow-[0_2px_10px_rgba(0,0,0,0.4)] hover:scale-[1.05] cursor-pointer lg:w-2/5 lg:order-1 lg:p-3">
-                <img src={fbIcon} alt="facebook" />
-                <p className="max-lg:hidden">Facebook</p>
+              <div className="relative bg-white/80 p-5 flex gap-2.5 justify-center rounded-[15px] shadow-[0_2px_10px_rgba(0,0,0,0.4)] overflow-hidden group hover:scale-[0.9] duration-500 [transition-timing-function:cubic-bezier(0.34,1.56,0.64,1)]  cursor-pointer lg:w-2/5 lg:order-1 lg:p-3">
+                <img
+                  src={fbIcon}
+                  alt="facebook"
+                  className="transition-all duration-500"
+                />
+                <p
+                  className="max-lg:hidden"
+                >
+                  Facebook
+                </p>
               </div>
             </div>
           </form>
