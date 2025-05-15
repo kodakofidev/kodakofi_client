0de70951a8a1dacf620cd022c9e6a0e27078a053
Imanul Aufa <imanulaufa@gmail.com>
2025-05-15 16:28:19 +0700

    feat(profile page): add profile page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePage() {
+  return (
+    <main className=' h-screen px-4 lg:px-8 md:px-12 xl:px-24'>
+        <h1 className='text-2xl font-semibold '>Profile</h1>
+    </main>
+  )
+}
+
+export default ProfilePage
