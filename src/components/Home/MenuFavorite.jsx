89749e36fd6660c5e2b9079cad5d43d7d90a22e8
mrpradana2 <mrpradana2@gmail.com>
2025-05-16 08:18:27 +0700

    feat(landing page): add products card in menu favorite section

--- src/components/Home/MenuFavorite.jsx
+++ src/components/Home/MenuFavorite.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import ProductCardHome from "./ProductCardHome";
+import Card from "../Card";
 
 export default function MenuFavorite() {
   return (
     <>
-      <section className="flex flex-col pb-10 px-4 pt-10 lg:px-8 md:px-12 xl:px-24 select-none">
+      <section className="flex flex-col pb-10 px-4 md:pt-14 lg:px-8 md:px-12 xl:px-24 select-none">
         <h1 className="text-3xl pb-5 text-center relative before:absolute before:bottom-0 before:content-[''] before:w-16 before:h-2 before:bg-amber-600 before:rounded-lg before:left-1/2 before:-translate-x-1/2">
           Here Is People <span className="text-brown">Favorite</span>
         </h1>
-        <p className="py-4 text-center">
+        <p className="pt-4 text-center">
           You can explore the menu that we provide with fun and have their own
           taste and make your day better.
         </p>
-        <div className="flex gap-2 flex-wrap h-full justify-between">
-          <ProductCardHome />
-          <ProductCardHome />
-          <ProductCardHome />
-          <ProductCardHome />
-          {/* <Card />
+        <div className="flex gap-4 mt-4 max-sm:overflow-x-scroll max-sm:overflow-y-hidden min-h-[540px]">
           <Card />
-          <Card /> */}
+          <Card />
+          <Card />
+          <Card />
         </div>
       </section>
     </>
