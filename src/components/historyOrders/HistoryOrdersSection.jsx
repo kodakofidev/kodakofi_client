f268c0872c66f282534ba266b522b42519daa48a
mrpradana2 <mrpradana2@gmail.com>
2025-05-16 19:07:29 +0700

    fix(checkout page): add filter date for history order

--- src/components/historyOrders/HistoryOrdersSection.jsx
+++ src/components/historyOrders/HistoryOrdersSection.jsx
@@ -6,7 +6,7 @@ export default function HistoryOrdersSection() {
     <>
       <section className="pt-6">
         <div className="grid grid-cols-5">
-          <div className="grid grid-cols-3 gap-2 col-span-5 px-2 py-2 bg-gray-200">
+          <div className="grid grid-cols-3 gap-2 col-span-5 md:col-span-3 px-2 py-2 bg-gray-200">
             <RadioStatusOrder
               id="onProgress"
               name="status"
@@ -23,8 +23,10 @@ export default function HistoryOrdersSection() {
               value="Finish Order"
             />
           </div>
-          <div className="grid col-span-2 ">
-            <div>okeeee</div>
+          <div className="grid col-span-2 md:col-span-2">
+            <div>
+              <input type="date" name="historyDate" id="historyDate" />
+            </div>
           </div>
         </div>
       </section>
