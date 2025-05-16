76ee61dee6fbf11d4f109c013de1758789a3ac4c
mrpradana2 <mrpradana2@gmail.com>
2025-05-16 14:35:57 +0700

    fix: fix import routing history order page

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 
 import ProductDetails from "./pages/product/ProductDetails";
 import Home from "./pages/Home";
-import CheckoutPage from "./pages/CheckoutPage";
+import CheckoutPage from "./pages/orders/CheckoutPage";
 import AuthRoutes from "./routing/auth";
 import MainLayout from "./layouts/MainLayouts";
 import Card from "./components/Card";
@@ -10,8 +10,6 @@ import Card from "./components/Card";
 import AdminLayouts from "./layouts/AdminLayouts";
 import ProfilePage from "./pages/ProfilePage";
 import HistoryOrderPage from "./pages/orders/HistoryOrderPage";
-
-// import ProductList from "./pages/products/ProductList";
 
 const Router = () => {
   return (
@@ -25,14 +23,10 @@ const Router = () => {
           <Route path="/" element={<Home />} />
 
           <Route path="/products" element={<Card />} />
-
-          {/* <Route path='/products/:id' element={<ProductDetails />} /> */}
-          {/* <Route path='/order/:id' element={<OrderDetails />} /> */}
           <Route path="/history" element={<HistoryOrderPage />} />
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
         </Route>
 
         {/* Layouting Admin */}
