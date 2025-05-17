ce8e3a52d253527fe8d1d1fe48131ce56b7ac165
mrpradana2 <mrpradana2@gmail.com>
2025-05-17 22:10:57 +0700

    fix(checkout page): fixed color text title list payment, responsive images payment methode, position text total, and modal payment methode

--- src/components/Checkout/TotalPayment.jsx
+++ src/components/Checkout/TotalPayment.jsx
@@ -57,40 +57,40 @@ export default function TotalPayment({
   return (
     <>
       <div className="py-4 md:pl-8 md:col-start-5 md:row-start-1 md:col-span-3 md:row-span-2">
-        <h1 className="text-xl font-semibold pb-4 md:pb-8">Total</h1>
+        <h1 className="text-xl font-semibold pb-4 md:pb-5 md:pt-3">Total</h1>
         <div className="grid grid-cols-2 gap-1 bg-gray-100 rounded-sm p-4">
-          <h1 className="font-semibold">Order</h1>
+          <h1 className="font-semibold text-[#4F5665]">Order</h1>
           <p className="place-self-end font-semibold">
             Idr. {totalOrderFormat}
           </p>
-          <h1>Delivery</h1>
+          <h1 className="font-semibold text-[#4F5665]">Delivery</h1>
           <p className="place-self-end font-semibold">
             Idr. {deliveryCostFormat}
           </p>
-          <h1 className="font-semibold">Tax</h1>
+          <h1 className="font-semibold text-[#4F5665]">Tax</h1>
           <p className="place-self-end font-semibold pb-5">Idr. {taxFormat}</p>
-          <h1 className="font-semibold">Sub Total</h1>
+          <h1 className="font-semibold text-[#4F5665]">Sub Total</h1>
           <p className="place-self-end font-semibold pb-2">
             Idr {subTotalFormat}
           </p>
-          <p
+          {/* <p
             className={`${
               !validationPaymentMethode ? "block" : "hidden"
             } block text-sm text-red-600 italic col-span-2`}
           >
             Payment methode cannot be empty
-          </p>
+          </p> */}
           <button
-            onClick={() => {
-              setPaymentMethodeModal(true);
-            }}
-            type="button"
+            // onClick={() => {
+            //   setPaymentMethodeModal(true);
+            // }}
+            type="submit"
             className="col-span-2 bg-orange py-1 rounded-sm hover:scale-[1.01] active:scale-[1] cursor-pointer select-none font-medium"
           >
             Checkout
           </button>
           <p className="col-span-2 py-2 text-gray-500">We Accept</p>
-          <div className="col-span-2 grid grid-cols-6 items-center">
+          <div className="col-span-2 grid grid-cols-6 items-center md:grid-cols-3 place-items-center gap-2 lg:grid-cols-6">
             <img src={BRI} alt="icon" />
             <img src={Dana} alt="icon" />
             <img src={BCA} alt="icon" />
