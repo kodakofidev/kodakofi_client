97e466153367e2c7cbba8da717525d8e87f8309d
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-05-18 17:34:54 +0700

    feat(order): add view order details

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router";
 
-import ProductDetails from "./pages/product/ProductDetails"
-import Home from "./pages/Home"
-import CheckoutPage from "./pages/orders/CheckoutPage"
-import AuthRoutes from "./routing/auth"
-import MainLayout from "./layouts/MainLayouts"
-import AdminLayouts from "./layouts/AdminLayouts"
-import ProfilePage from "./pages/ProfilePage"
-import HistoryOrderPage from "./pages/orders/HistoryOrderPage"
-import ProductList from "./pages/product/ProductList"
+import ProductDetails from "./pages/product/ProductDetails";
+import Home from "./pages/Home";
+import CheckoutPage from "./pages/orders/CheckoutPage";
+import AuthRoutes from "./routing/auth";
+import MainLayout from "./layouts/MainLayouts";
+import AdminLayouts from "./layouts/AdminLayouts";
+import ProfilePage from "./pages/ProfilePage";
+import HistoryOrderPage from "./pages/orders/HistoryOrderPage";
+import ProductList from "./pages/product/ProductList";
+import OrderDetails from "./pages/orders/OrderDetails";
 
 const Router = () => {
   return (
@@ -19,21 +20,22 @@ const Router = () => {
 
         {/* Main Layouts */}
         <Route element={<MainLayout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/products' element={<ProductList />} />
-          <Route path='/products/:id' element={<ProductDetails />} />
-          <Route path='/history' element={<HistoryOrderPage />} />
-          <Route path='/checkout' element={<CheckoutPage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-          <Route path='/products/:id' element={<ProductDetails />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/history" element={<HistoryOrderPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/order/:id" element={<OrderDetails />} />
         </Route>
         {/* Layouting Admin */}
-        <Route path='admin' element={<AdminLayouts />}>
+        <Route path="admin" element={<AdminLayouts />}>
           {/* <Route index element={<Dashboard />}></Route> */}
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
