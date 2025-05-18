b7c7eb4ef4cdd6c2a5c3aec828a100a52218816f
mrpradana2 <mrpradana2@gmail.com>
2025-05-18 08:49:04 +0700

    fix(history order page): handling pagination multiple page, padding in number 2, radio button progress

--- src/pages/orders/HistoryOrderPage.jsx
+++ src/pages/orders/HistoryOrderPage.jsx
@@ -7,11 +7,11 @@ export default function HistoryOrderPage() {
     <>
       <section className="px-4 lg:px-8 md:px-12 xl:px-24 pt-[90px]">
         <div className="grid grid-cols-1">
-          <div className="flex gap-3 justify-between items-center md:justify-normal">
+          <div className="flex gap-5 justify-between items-center md:justify-normal">
             <h1 className="text-3xl">History Order</h1>
             <div className="w-6 h-6 bg-gray-200 leading-6 text-center">2</div>
           </div>
-          <div className="grid grid-cols-1 gap-y-4 md:gap-4 lg:grid-cols-7">
+          <div className="grid grid-cols-1 gap-y-4 min-[1025px]:gap-4 lg:grid-cols-7">
             <HistoryOrdersSection />
             <SendMessage />
           </div>
